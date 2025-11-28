6dcd81874203a9095ba6590bf0ca8e3ddef6471d
gabrieladutra <gabriela_ddutra@outlook.com>
2025-11-28 13:44:26 -0300

    Correção da disposição dos itens menu semanal

--- cardapio-sesc/frontend/src/Home.jsx
+++ cardapio-sesc/frontend/src/Home.jsx
@@ -8,31 +8,32 @@ export default function Home() {
 
   return (
     <div className="w-full min-h-screen bg-white flex flex-col items-center">
-      <h1 className="text-yellow-600 text-2xl mt-4">MENU DIÁRIO</h1>
+      <h1 className="text-yellow-600 text-3xl md:text-4xl mt-4">MENU DIÁRIO</h1>
 
-      <div className="flex flex-col gap-8 mt-8 items-center"> {/* items-center aqui */}
+      <div className="flex flex-col md:flex-row  gap-8 mt-8 items-center"> 
         <div className="flex flex-col items-center">
-          <h2 className="text-blue-950 font-semibold text-center mb-2">Restaurante</h2>
-          <div className="border-2 border-blue-950 rounded-md p-4 w-40 whitespace-pre-line text-center">
+          <h2 className="text-blue-950 font-semibold text-center mb-2 md:text-2xl">Restaurante</h2>
+          <div className="border-2 border-blue-950 rounded-md p-4 w-40 md:p-2 md:pt-10 md:w-80 md:h-80  whitespace-pre-line text-center md:text-2xl">
             {cardapio}
           </div>
         </div>
 
         <div className="flex flex-col items-center">
-          <h2 className="text-blue-950 font-semibold text-center mb-2">Lanchonete</h2>
-          <div className="border-2 border-blue-950 rounded-md p-4 w-40 whitespace-pre-line text-center">
-            {cardapio}
+          <h2 className="text-blue-950 font-semibold text-center mb-2 md:text-2xl">Lanchonete</h2>
+          <div className="border-2 border-blue-950 rounded-md p-4 md:p-2 md:pt-10 w-40 md:w-80 md:h-80 whitespace-pre-line text-center md:text-2xl">
+                {cardapio}
           </div>
         </div>
 
-        <button
+        
+      </div>
+      <button
           onClick={() => navigate("/menu-semanal")}
-          className="rounded-md bg-green-700 text-white h-11 px-4 flex items-center gap-2"
+          className="rounded-md bg-green-700 text-white h-11 px-4 flex items-center gap-2 cursor-pointer md:mb-8 mt-10"
         >
           <CircleChevronRight />
           Menu Semanal
         </button>
-      </div>
     </div>
   );
 }
--- cardapio-sesc/frontend/src/MenuSemanal.jsx
+++ cardapio-sesc/frontend/src/MenuSemanal.jsx
@@ -11,7 +11,7 @@ export default function MenuSemanal() {
 
             <h1 className="text-yellow-600 text-2xl mt-4">MENU SEMANAL</h1>
 
-            <div className="flex flex-col gap-8 mt-8 items-center">
+            <div className="flex flex-col md:flex-row gap-8 mt-8 items-center">
 
                 <div className="flex flex-col items-center">
                     <h2 className="text-blue-950 font-semibold mb-2 text-center">Segunda-feira</h2>
@@ -34,9 +34,23 @@ export default function MenuSemanal() {
                     </div>
                 </div>
 
+                <div className="flex flex-col items-center">
+                    <h2 className="text-blue-950 font-semibold mb-2 text-center">Quinta-feira</h2>
+                    <div className="border-2 border-blue-950 rounded-md p-4 w-40 whitespace-pre-line text-center">
+                        {cardapio}
+                    </div>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <h2 className="text-blue-950 font-semibold mb-2 text-center">Sexta-feira</h2>
+                    <div className="border-2 border-blue-950 rounded-md p-4 w-40 whitespace-pre-line text-center">
+                        {cardapio}
+                    </div>
+                </div>
+
                 <button
                     onClick={() => navigate("/")}
-                    className="rounded-md bg-green-700 text-white h-11 px-4 flex items-center gap-2"
+                    className="rounded-md bg-green-700 text-white h-11 px-4 flex items-center gap-2 cursor-pointer"
                 >
                     <CircleArrowLeft />
                     Voltar ao Menu Diário
