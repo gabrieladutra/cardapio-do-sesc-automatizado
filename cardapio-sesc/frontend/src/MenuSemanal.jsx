598006cd8d8d78c540e538198f19f2c25c962c86
gabrieladutra <gabriela_ddutra@outlook.com>
2025-12-11 19:48:15 -0300

    WIP

--- cardapio-sesc/frontend/src/MenuSemanal.jsx
+++ cardapio-sesc/frontend/src/MenuSemanal.jsx
@@ -2,6 +2,12 @@ import { CircleArrowLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
     import { useEffect, useState } from "react";
 
+function capitalized(word) {
+    if (!word) return "";
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+
 export default function MenuSemanal() {
     const [menuRestauranteSeg, setMenuRestauranteSeg] = useState("");
      const [menuRestauranteTer, setMenuRestauranteTer] = useState("");
@@ -31,7 +37,7 @@ export default function MenuSemanal() {
         setMenuRestauranteQui(dados.restaurante[3][0].texto.S);
         setMenuRestauranteSext(dados.restaurante[4][0].texto.S)
 
-        setMenuLanchoneteSeg(dados.lanchonete[0][0].texto.S.toLocaleLowerCase());
+        setMenuLanchoneteSeg(dados.lanchonete[0][0].texto.S);
         setMenuLanchoneteTer(dados.lanchonete[1][0].texto.S);
         setMenuLanchoneteQua(dados.lanchonete[2][0].texto.S);
         setMenuLanchoneteQui(dados.lanchonete[3][0].texto.S);
@@ -45,62 +51,64 @@ export default function MenuSemanal() {
     return (
         <div className="w-full min-h-screen bg-white flex flex-col items-center">
 
-            <h1 className="text-yellow-600 text-2xl mt-4 md:text-3xl">MENU SEMANAL</h1>
+            <h1 className="text-yellow-600 text-2xl mt-4 md:text-4xl">MENU SEMANAL</h1>
+            <h1 className="text-yellow-600 text-2xl mt-8 md:text-2x1">Restaurante</h1>
 
             <div className="flex flex-col md:flex-row gap-8 mt-8 items-center">
 
                 <div className="flex flex-col items-center">
                     <h2 className="text-blue-950 font-semibold mb-2 text-center md:text-lg">Segunda-feira</h2>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                         {menuRestauranteSeg}
                     </div>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-8 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-20 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                         {menuLanchoneteSeg}
                     </div>
                 </div>
 
                 <div className="flex flex-col items-center">
                     <h2 className="text-blue-950 font-semibold mb-2 text-center md:text-lg">Terça-feira</h2>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                         {menuRestauranteTer}
                     </div>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-8 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-20 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                         {menuLanchoneteTer}
                     </div>
                 </div>
 
                 <div className="flex flex-col items-center">
                     <h2 className="text-blue-950 font-semibold mb-2 text-center md:text-lg">Quarta-feira</h2>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                     {menuRestauranteQua}
                     </div>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-8 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-20 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                         {menuLanchoneteQua}
                     </div>
                 </div>
                 <div className="flex flex-col items-center">
                     <h2 className="text-blue-950 font-semibold mb-2 text-center md:text-lg">Quarta-feira</h2>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                     {menuRestauranteQui}
                     </div>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-8 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-20 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                         {menuLanchoneteQui}
                     </div>
                 </div>
                 <div className="flex flex-col items-center">
                     <h2 className="text-blue-950 font-semibold mb-2 text-center md:text-lg">Quarta-feira</h2>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                     {menuRestauranteSext}
                     </div>
-                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-8 w-40 whitespace-pre-line text-center md:h-60 md:w-60 md:text-md">
+                    <div className="border-2 border-blue-950 rounded-md pt-2 px-2 mt-20 w-40 whitespace-pre-line text-center md:h-60 md:w-80 md:text-lg">
                         {menuLanchoneteSext}
                     </div>
                 </div>
 
             </div>
+            
             <button
                     onClick={() => navigate("/")}
-                    className="rounded-md bg-green-700 text-white h-11 px-4 flex items-center gap-2 cursor-pointer mt-10 mb-4"
+                    className="rounded-md bg-green-700 text-white h-11 px-4 flex items-center gap-3 cursor-pointer mt-10 mb-4"
                 >
                     <CircleArrowLeft />
                     Voltar ao Menu Diário
