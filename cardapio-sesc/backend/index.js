cd2a7984a71edab34a598f87999fcb7f91d27170
gabrieladutra <gabriela_ddutra@outlook.com>
2026-02-24 11:59:59 -0300

    Atualizando dependências

--- cardapio-sesc/backend/index.js
+++ cardapio-sesc/backend/index.js
@@ -1,7 +1,7 @@
 import { DynamoDBClient, PutItemCommand, QueryCommand } from "@aws-sdk/client-dynamodb"
 import { parse } from "node-html-parser"
 import { createWorker } from "tesseract.js"
-import Jimp from "jimp"
+import { Jimp } from "jimp"
 
 const dynamo = new DynamoDBClient({ region: "sa-east-1" })
 
